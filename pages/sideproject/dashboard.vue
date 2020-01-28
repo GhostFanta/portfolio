5e61bb42a9d24c7afa1b987036cebee728616364
GhostFanta <huiyueguan111@gmail.com>
2020-01-28 00:19:59 -0500

    New side card

--- pages/sideproject/dashboard.vue
+++ pages/sideproject/dashboard.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="z-dashboard">
     <div class="z-title">Dashboard</div>
+    <span class="badge badge-info">React.js</span>
+    <span class="badge badge-info">Redux</span>
+    <span class="badge badge-info">React-router</span>
+    <span class="badge badge-warning">axios</span>
+    <span class="badge badge-warning">firebase</span>
+    <span class="badge badge-primary">bootstrap</span>
+    <h5 class="z-subtitle z-intro">
+      <strong>The dashboard</strong> is a dashboard project which integrates
+      multiple sections serving different functionalities including analytics,
+      profile management, products management and so on.
+    </h5>
+    <a href="https://flamboyant-swartz-5bcde4.netlify.com/login"
+      >Visit the demo (still under development)</a
+    >
+    <p>
+      You can checkout the source code on
+      <a href="https://github.com/GhostFanta/dashboard.react.zc">Github</a>
+    </p>
   </div>
 </template>
 <script>
